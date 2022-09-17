9c741e22c636dd7564f4ac1c3c7aa5be2cc7f991
José Rodrigues <jose15.luis00@gmail.com>
2022-09-17 18:44:35 +0100

    feat: games ads page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,6 +9,7 @@ import {
 } from "@expo-google-fonts/inter";
 import { Home } from "./src/screens/Home";
 import { Loading } from "./src/components/Loading";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -26,7 +27,7 @@ export default function App() {
         translucent
       />
 
-      {fontsLoaded ? <Home /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </Background>
   );
 }
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,30 +1,54 @@
-import React from "react";
-import { View, Image, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { Image, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import logo from "../../assets/logo-nlw-esports.png";
-import { GameCard } from "../../components/GameCard";
+import { Background } from "../../components/Background";
+import { GameCard, GameCardProps } from "../../components/GameCard";
 import { Heading } from "../../components/Heading";
-import { GAMES } from "../../utils/games";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  const navigation = useNavigation();
+
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate("game", { id, title, bannerUrl });
+  }
+
+  useEffect(() => {
+    fetch("http://192.168.1.5:3333/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data));
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo} />
+    <Background>
+      <SafeAreaView style={styles.container}>
+        <Image source={logo} style={styles.logo} />
 
-      <Heading
-        title="Encontre o seu duo"
-        subtitle="Selecione o game que deseja jogar..."
-      />
+        <Heading
+          title="Encontre o seu duo"
+          subtitle="Selecione o game que deseja jogar..."
+        />
 
-      <FlatList
-        contentContainerStyle={styles.contentList}
-        data={GAMES}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <GameCard data={item} />}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-      />
-    </View>
+        <FlatList
+          contentContainerStyle={styles.contentList}
+          data={games}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+              onPress={() => {
+                handleOpenGame(item);
+              }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </SafeAreaView>
+    </Background>
   );
 }
